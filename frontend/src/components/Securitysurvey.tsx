ab58586fb61482845150cd69c7c9674054fde394
saran <saransathi1010@gmail.com>
2025-03-23 22:14:47 +0530

    v2 d

--- frontend/src/components/Securitysurvey.tsx
+++ frontend/src/components/Securitysurvey.tsx
@@ -4,7 +4,7 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 import { AlertTriangle, CheckCircle, Clock, Download, Send, FileText, PieChart as  BarChart2 } from 'lucide-react';
 
 // Define the base URL for API requests
-const API_BASE_URL = 'https://securebackend.saransathish.click:8000/api'; 
+const API_BASE_URL = 'https://securebackend.saransathish.click/api'; 
 
 // const API_BASE_URL = 'http://localhost:5000/api'; 
 
